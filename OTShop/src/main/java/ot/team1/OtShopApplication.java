e169b9b0bf5cb78f2451efae6be79dbb65d88e0a
Chicken Head <Chicken Head@DESKTOP-6RVSQM3>
2023-12-29 13:05:42 +0900

    first commit

---
+++ OTShop/src/main/java/ot/team1/OtShopApplication.java
@@ -0,0 +1,13 @@
+package ot.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtShopApplication.class, args);
+	}
+
+}
---
+++ OTShop/src/test/java/ot/team1/OtShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ot.team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
